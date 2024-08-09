f18d8dd0b7305ff7738d46b8d130361078d31b63
phudeohi <141209248+phudeohi@users.noreply.github.com>
2024-08-09 16:13:54 +0700

    Model MyDbContext

---
+++ MyDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CarDealer.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Part> Parts { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+    }
+}
