ad4cb6c64bc19c9e0b17da23b000d5920fe259c8
Quang161 <quangtrn1612003@gmail.com>
2024-08-09 15:48:05 +0700

    Add my project files

---
+++ CarDealer/Controllers/PartsController.cs
@@ -0,0 +1,59 @@
+﻿using CarDealer.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarDealer.Controllers
+{
+    public class PartsController : Controller
+    {
+        private readonly MyDbContext _context;
+
+        public PartsController(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var parts = await _context.Parts
+                .Select(p => new PartViewModel
+                {
+                    Name = p.Name,
+                    Price = p.Price,
+                    Quantity = p.Quantity,
+                    SupplierId = p.SupplierId
+                })
+                .ToListAsync();
+
+            return View(parts);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(PartViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var part = new Part
+                {
+                    Name = model.Name,
+                    Price = model.Price,
+                    Quantity = model.Quantity,
+                    SupplierId = model.SupplierId
+                };
+
+                _context.Parts.Add(part);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(model);
+        }
+    }
+}
